05672d73a14ca5da181b759d04e13afbc1ba174b
edoardolombardi <edoardo.lombardi@optimad.it>
2016-01-26 14:59:27 +0100

    Bug fixing in computePartition

--- test/test_find_bug_LB.cpp
+++ test/test_find_bug_LB.cpp
@@ -5,13 +5,11 @@
  *      Author: marco
  */
 
-
-
-
 #include "preprocessor_defines.dat"
 #include "Class_Global.hpp"
 #include "Class_Para_Tree.hpp"
 #include "User_Data_Comm.hpp"
+#include "User_Data_LB.hpp"
 
 using namespace std;
 
@@ -50,6 +48,10 @@ int main(int argc, char *argv[]) {
                 /**<Define vectors of data.*/
                 uint32_t nocts = pablo.getNumOctants();
                 uint32_t nghosts = pablo.getNumGhosts();
+                vector<double> weight;
+                vector<double> weight2;
+                vector<double> weight_ghosts;
+
 
                 /**<Set marker inside circle*/
                 for (int i=0; i<nocts; i++){
@@ -62,6 +64,7 @@ int main(int argc, char *argv[]) {
                                 }
                         }
                 }
+
                 pablo.adapt();
                 if(pablo.rank==3 || pablo.rank==4)
                         pablo.setMarker((uint32_t)0,1);
@@ -75,13 +78,62 @@ int main(int argc, char *argv[]) {
                 if(pablo.rank==3)
                         pablo.setMarker((uint32_t)0,1);
                 pablo.adapt();
+                nocts = pablo.getNumOctants();
+                cout << pablo.rank << " nocts " << nocts << endl;
+                weight.resize(nocts, 1.0);
+                weight2.resize(nocts, 1.0);
+                for (int i=0; i<nocts; i++){
+                        vector<vector<double> > nodes = pablo.getNodes(i);
+                        for (int j=0; j<global2D.nnodes; j++){
+                                double x = nodes[j][0];
+                                double y = nodes[j][1];
+                                if ((pow((x-xc),2.0)+pow((y-yc),2.0) <= pow(radius,2.0))){
+                                    weight[i] = 2;
+                                    weight2[i] = 2;
+                                }
+                        }
+                }
+                double sum = 0.0;
+                for (int i=0; i<nocts; i++){
+                	sum += weight[i];
+                }
+                cout << pablo.rank << " nocts " << nocts << endl;
+                cout << pablo.rank << " sum weight " << sum << endl;
+
+
 #if NOMPI==0
-                pablo.loadBalance();
+                User_Data_LB<vector<double> > data_lb(weight, weight_ghosts);
+				pablo.loadBalance(data_lb, &weight2);
+				weight2 = weight;
+                nocts = pablo.getNumOctants();
+                for (int i=0; i<nocts; i++){
+                        vector<vector<double> > nodes = pablo.getNodes(i);
+                        for (int j=0; j<global2D.nnodes; j++){
+                                double x = nodes[j][0];
+                                double y = nodes[j][1];
+                                if ((pow((x-xc),2.0)+pow((y-yc),2.0) <= pow(radius,2.0))){
+                                    weight[i] = 5;
+                                    weight2[i] = 5;
+                                }
+                        }
+                }
+				pablo.loadBalance(data_lb, &weight2);
+//				pablo.loadBalance(data_lb);
 #endif
+
+                nocts = pablo.getNumOctants();
+                sum = 0.0;
+                for (int i=0; i<nocts; i++){
+                	sum += weight[i];
+                }
+                cout << pablo.rank << " nocts " << nocts << endl;
+                cout << pablo.rank << " size weight " << weight.size() << endl;
+                cout << pablo.rank << " sum weight " << sum << endl;
+
                 /**<Update the connectivity and write the para_tree.*/
                 iter = 0;
                 pablo.updateConnectivity();
-                pablo.write("PabloLB_iter"+to_string(static_cast<unsigned long long>(iter)));
+                pablo.writeTest("PabloLB_iter"+to_string(static_cast<unsigned long long>(iter)), weight);
 
 #if NOMPI==0
         }
